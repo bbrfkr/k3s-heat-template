a8a8f1e171dc4cc367bc47eeadd8cfc3b2d087bc
bbrfkr <1.boogie.5.sophia.4.pop.0@gmail.com>
2019-03-13 23:17:02 +0900

    add service cidr and cluster dns

--- fragments/configure_k3s_master.sh
+++ fragments/configure_k3s_master.sh
@@ -2,8 +2,12 @@
 
 source /etc/heat_params
 
+SERVER_OPTION=""
 if [ $DISABLE_MASTER_AGENT = "True" ]; then
-  cat <<EOF > /etc/systemd/system/k3s-master.service
+  SERVER_OPTION="--disable-agent"
+fi
+
+cat <<EOF > /etc/systemd/system/k3s-master.service
 [Unit]
 Description=Lightweight Kubernetes
 Documentation=https://k3s.io
@@ -12,7 +16,7 @@ After=network.target
 [Service]
 ExecStartPre=-/sbin/modprobe br_netfilter
 ExecStartPre=-/sbin/modprobe overlay
-ExecStart=/usr/bin/k3s server --node-ip ${K3S_MASTER_PUBLIC_IP} --cluster-cidr ${PODS_NETWORK_CIDR} --disable-agent
+ExecStart=/usr/bin/k3s server --node-ip ${K3S_MASTER_PUBLIC_IP} --cluster-cidr ${PODS_NETWORK_CIDR} --service-cidr ${SERVICE_NETWORK_CIDR} --cluster-dns ${CLUSTER_DNS_IP} ${SERVER_OPTION}
 KillMode=process
 Delegate=yes
 LimitNOFILE=infinity
@@ -23,30 +27,6 @@ TasksMax=infinity
 [Install]
 WantedBy=multi-user.target
 EOF
-fi
-
-if [ $DISABLE_MASTER_AGENT = "False" ]; then
-  cat <<EOF > /etc/systemd/system/k3s-master.service
-[Unit]
-Description=Lightweight Kubernetes
-Documentation=https://k3s.io
-After=network.target
-
-[Service]
-ExecStartPre=-/sbin/modprobe br_netfilter
-ExecStartPre=-/sbin/modprobe overlay
-ExecStart=/usr/bin/k3s server --node-ip ${K3S_MASTER_PUBLIC_IP} --cluster-cidr ${PODS_NETWORK_CIDR}
-KillMode=process
-Delegate=yes
-LimitNOFILE=infinity
-LimitNPROC=infinity
-LimitCORE=infinity
-TasksMax=infinity
-
-[Install]
-WantedBy=multi-user.target
-EOF
-fi
 
 systemctl daemon-reload
 systemctl enable k3s-master
